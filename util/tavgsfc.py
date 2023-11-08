536c016e161c294516ee333a5721962c4868218a
Jesus Fernandez <Jesus.Fernandez@unican.es>
2023-11-08 11:06:54 +0100

    Fix to add FLAG_TAVGSFC
    and other variable attributes to TAVGSFC

--- util/tavgsfc.py
+++ util/tavgsfc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# tavgsfc.py v0.1 (2023-11-07)
+# tavgsfc.py v0.2 (2023-11-08)
 #
 # Inserts running mean lowest model level temperatures into WRF met_em
 # files. The new variable TAVGSFC is used by WRF as skin temperature
@@ -43,14 +43,25 @@ def process_wrf_files(pattern, overwrite):
         continue
     else:
       tavgsfc = input_file.createVariable(varname, 'f4', ('Time', 'south_north', 'west_east'))
+      for attr_name in input_file.variables['TT'].ncattrs():
+        setattr(tavgsfc, attr_name, getattr(input_file.variables['TT'], attr_name))
+      setattr(tavgsfc, 'MemoryOrder', 'XY ')
       tavgsfc[:] = running_mean
+    # Add the global attribute flag
+    input_file.setncattr('FLAG_TAVGSFC', 1)
     input_file.close()
   
 
 def main():
-  parser = argparse.ArgumentParser(description='Process WRF met_em files and add running mean temperature')
-  parser.add_argument('pattern', help='File pattern to match met_em files')
-  parser.add_argument('-O', '--overwrite', action='store_true', help='Overwrite TAVGSFC if it already exists')
+  parser = argparse.ArgumentParser(
+    description='Process WRF met_em files and add running mean temperature'
+  )
+  parser.add_argument('pattern',
+    help='File pattern to match met_em files'
+  )
+  parser.add_argument('-O', '--overwrite', action='store_true',
+    help='Overwrite TAVGSFC if it already exists'
+  )
   args = parser.parse_args()
   process_wrf_files(args.pattern, args.overwrite)
 
